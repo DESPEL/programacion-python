ae048e38eb6b5aa655b015ea381e068c58b4ea8c
DESPEL <52083283+DESPEL@users.noreply.github.com>
2020-03-07 18:15:15 -0800

    Removing unused files

--- P3 rpg/sprites.py
+++
@@ -1,5 +0,0 @@
-from image_loader import load_console_sprite
-
-warrior = load_console_sprite('sprites/warrior.png')
-wizard = load_console_sprite('sprites/wizard.png')
-archer = load_console_sprite('sprites/archer.png')
